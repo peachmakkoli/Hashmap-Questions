c771fabab8a4e16e1bfea1e86f2c901083c1c24d
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 12:27:08 -0700

    amends pseudocode, checks if strings are equal or not of same length

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,7 +1,9 @@
 
 def permutations?(string1, string2)
-  # return false if the strings are not the same length
-  # define which string is smaller/larger
-  # add each char from the smaller string to the hash table
-  # lookup each char in the larger string in the hash table; if it's found,
+  # return true if strings are equal; return false if the strings are not the same length
+  return true if string1 == string2
+  return false if string1.length != string2.length
+
+  # add each char from string1 to the hash table
+  # lookup each char in string2 in the hash table; if it's found, return true; else return false
 end
