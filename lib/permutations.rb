a76ff642623efec7fd48f133e5b3cf0bc2ece7c4
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 12:23:53 -0700

    adds pseudocode

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,7 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  # return false if the strings are not the same length
+  # define which string is smaller/larger
+  # add each char from the smaller string to the hash table
+  # lookup each char in the larger string in the hash table; if it's found,
 end
