fdddac4ce1a839dc40b9fa6dc42933583afd679f
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 11:31:03 -0700

    adds pseudocode

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,5 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # Add each element from the smaller array to the hash table. 
+  # Lookup each element in the larger array in the hash table. 
+  # If it’s found in the hash table, add it to the results array.
 end
