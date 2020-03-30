a1c47961df2ee77cb83758c4f406c08aff3edbfc
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 15:27:53 -0700

    fixes mislabeled it block and adds follow-up test for when the number of specific letters are different

--- test/permutations_test.rb
+++ test/permutations_test.rb
@@ -13,21 +13,16 @@ describe "permutations?" do
     expect(permutations?("pasta", "atsap")).must_equal true
   end
 
-  it "returns true for 'pizza', 'pizza'" do
-    expect(permutations?("pizza", "pizza")).must_equal true
-  end
-
   it "returns false for 'pizza', 'pasta'" do
     expect(permutations?("pizza", "pasta")).must_equal false
   end
 
-  # this test was passing superficially because my method returned false if the strings were different lengths; I modified the second test string so it would still meet the conditions but not trigger line 4 in the method
+  # this test was passing superficially because of line 4 in my method; I added a follow-up test below
   it "returns false if the number of a specific letter are different" do
-    expect(permutations?("pizza", "pizaa")).must_equal false
-  end
-  
-  # see above comment
-  it "returns false if the strings are of different lengths" do
     expect(permutations?("pizza", "piza")).must_equal false
+  end
+
+  it "returns false if the number of a specific letter are different when the strings lengths are the same" do
+    expect(permutations?("pizza", "pizaa")).must_equal false
   end
 end
