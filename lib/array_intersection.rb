ad47667674d54c6792113fa3eb148c64182a3531
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 12:16:33 -0700

    defines intersection method, all tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,5 +1,22 @@
 def intersection(list1, list2)
-  # Add each element from the smaller array to the hash table. 
-  # Lookup each element in the larger array in the hash table. 
-  # If it’s found in the hash table, add it to the results array.
+  # define which array is the smallest and which is the largest
+  list1.length < list2.length ? (smaller, larger = list1, list2) : (smaller, larger = list2, list1)
+  
+  # Add each element from the smaller array to the hash table
+  lookup_hash = {}
+  smaller.each do |num|
+    if lookup_hash[num]
+      lookup_hash[num] = false
+    else
+      lookup_hash[num] = true
+    end
+  end
+
+  # Lookup each element in the larger array in the hash table; if it’s found in the hash table, add it to the results array
+  results = []
+  larger.each do |num|
+    results << num if lookup_hash[num]
+  end
+
+  return results
 end
