cb8e1693aa233987ac252a08a00fc35dbe9a5f2c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 13:03:25 -0700

    adds each char in string to the hash table, works for even-length palindromes

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,6 +1,20 @@
 
 def palindrome_permutation?(string)
-  # return true if the string is empty
+  return true if string.empty?
+  
+  # adds each char in string to the hash table
+  lookup_hash = {}
+  string.each_char do |char|
+    if lookup_hash[char]
+      lookup_hash[char] = false
+    else
+      lookup_hash[char] = true
+    end
+  end
+
   # if the string has an even number of characters, the palindrome should have two of each letter
+  if (string.length).even?
+    return true if lookup_hash.length == string.length / 2
+  end
   # if the string has an odd number of characters, the palindrome should have only one odd letter out
 end
