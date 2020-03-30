509e6aec1f4fbc0dd64d8ea909d7d1938697c974
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 12:48:27 -0700

    adds pseudocode and comments on logic

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,6 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  # return true if the string is empty
+  # if the string has an even number of characters, the palindrome should have two of each letter
+  # if the string has an odd number of characters, the palindrome should have only one odd letter out
 end
