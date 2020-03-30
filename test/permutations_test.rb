0fce2d8705176f6232e43c683b92c55c13fde23c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 13:20:59 -0700

    modifies test for 'returns false if the number of a specific letter are different', see comments

--- test/permutations_test.rb
+++ test/permutations_test.rb
@@ -21,7 +21,13 @@ describe "permutations?" do
     expect(permutations?("pizza", "pasta")).must_equal false
   end
 
+  # this test was passing superficially because my method returned false if the strings were different lengths; I modified the second test string so it would still meet the conditions but not trigger line 4 in the method
   it "returns false if the number of a specific letter are different" do
+    expect(permutations?("pizza", "pizaa")).must_equal false
+  end
+  
+  # see above comment
+  it "returns false if the strings are of different lengths" do
     expect(permutations?("pizza", "piza")).must_equal false
   end
 end
