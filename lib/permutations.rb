779eac3a09ec04487cb51950af0286a04b77fe6c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 12:29:49 -0700

    adds each char from string1 to the hash table

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -5,5 +5,13 @@ def permutations?(string1, string2)
   return false if string1.length != string2.length
 
   # add each char from string1 to the hash table
+  lookup_hash = {}
+  string1.each_char do |char|
+    if lookup_hash[char]
+      lookup_hash[char] = false
+    else
+      lookup_hash[char] = true
+    end
+  end
   # lookup each char in string2 in the hash table; if it's found, return true; else return false
 end
