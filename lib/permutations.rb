95e846498f5bed294cdcf0c8431446245bf6766c
peachmakkoli <sara.lee.higgins@gmail.com>
2020-03-30 16:16:13 -0700

    counts how many of each letter are in each string, compares hash tables, passes all tests!

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -5,16 +5,33 @@ def permutations?(string1, string2)
   
   # add each char from string1 to the hash table
   lookup_hash = {}
+  letter_count1 = {}
   string1.each_char do |char|
     if lookup_hash[char]
       lookup_hash[char] = false
     else
       lookup_hash[char] = true
     end
+
+    if letter_count1[char]
+      letter_count1[char] += 1
+    else
+      letter_count1[char] = 0
+    end
   end
 
-  return false if string1 != string2 && string1.squeeze.length == string2.squeeze.length # returns false if the number of a specific letter are different, but the string lengths are the same
-  
+  letter_count2 = {}
+  string2.each_char do |char|
+    if letter_count2[char]
+      letter_count2[char] += 1
+    else
+      letter_count2[char] = 0
+    end
+  end
+
+  # returns false if the number of a specific letter are different, but the string lengths are the same
+  return false if letter_count1 != letter_count2
+
   # lookup each char in string2 in the hash table; if the value of lookup_hash[num] is nil, return false; else return true
   string2.each_char do |char|
     return false if lookup_hash[char].nil?
