0afdae41bdbd02f915729cc3949e01158f5e41de
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-02 22:47:53 -0700

    simplifies code

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -3,11 +3,8 @@ def permutations?(string1, string2)
   return false if string1.length != string2.length
   
   # add each char from string1 to the hash table
-  lookup_hash = {}
   letter_count1 = {}
   string1.each_char do |char|
-    lookup_hash[char] ? lookup_hash[char] = false : lookup_hash[char] = true
-
     letter_count1[char] ? letter_count1[char] += 1 : letter_count1[char] = 0 # counts how many of each letter are in string1
   end
 
@@ -21,7 +18,7 @@ def permutations?(string1, string2)
 
   # lookup each char in string2 in the hash table
   string2.each_char do |char|
-    return false if lookup_hash[char].nil?
+    return false if letter_count1[char].nil?
   end
 
   return true
